15c0d9422e8ffc6ccd85c656a5a228d9943d6c5b
katianne23 <katianne40@gmail.com>
2026-03-21 16:57:19 -0300

    feat: created header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-function App() {
-  
+import { Logo } from "./components/icons/Logo";
+import { More } from "./components/icons/More";
+import "./App.css";
+import "./Index.css";
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <div className="app">
+      <header className="app-header">
+        <div className="container">
+          <div className="header-content">
+            <div className="logo">
+              <Logo />
+              <h1>People CRUD</h1>
+            </div>
+            <button className="btn-primary btn-add">
+              <More />
+              Adicionar Pessoa
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
